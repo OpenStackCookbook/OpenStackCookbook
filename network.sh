2682de0b15b49218161f4239558b86e3514f5eea
Kevin Jackson <kevin@linuxservices.co.uk>
2013-05-26 07:47:25 +0100

    Updates for Chapter 10, WIP

---
+++ network.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# network.sh
+
+# Authors: Kevin Jackson (kevin@linuxservices.co.uk)
+#          Cody Bunch (bunchc@gmail.com)
+
+# Source in common env vars
+. /vagrant/common.sh
+
+# The routeable IP of the node is on our eth1 interface
+MY_IP=$(ifconfig eth1 | awk '/inet addr/ {split ($2,A,":"); print A[2]}')
+

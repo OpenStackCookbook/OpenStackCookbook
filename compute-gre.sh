e8184d0e1e169725744f2c842e85ab9171c3e5bc
Cody Bunch <bunchc@gmail.com>
2013-10-26 15:35:03 -0500

    sed quantum neutron

--- compute-gre.sh
+++ compute-gre.sh
@@ -77,15 +77,15 @@ sudo ip link set eth3 promisc on
 # Assign IP to br-ex so it is accessible
 sudo ifconfig br-ex $ETH3_IP netmask 255.255.255.0
 
-# Quantum
-sudo apt-get install -y quantum-plugin-openvswitch-agent python-cinderclient
+# Neutron
+sudo apt-get install -y neutron-plugin-openvswitch-agent python-cinderclient
 
-# Configure Quantum
-# /etc/quantum/plugins/openvswitch/ovs_quantum_plugin.ini
+# Configure Neutron
+# /etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini
 echo "
 [DATABASE]
 reconnect_interval = 2
-sql_connection=mysql://quantum:openstack@${CONTROLLER_HOST}/quantum
+sql_connection=mysql://neutron:openstack@${CONTROLLER_HOST}/neutron
 [AGENT]
 # Agent's polling interval in seconds
 polling_interval = 2
@@ -96,26 +96,26 @@ integration_bridge=br-int
 tunnel_bridge=br-tun
 local_ip=${MY_IP}
 enable_tunneling=True
-root_helper = sudo /usr/bin/quantum-rootwrap /etc/quantum/rootwrap.conf
+root_helper = sudo /usr/bin/neutron-rootwrap /etc/neutron/rootwrap.conf
 [SECURITYGROUP]
-# Firewall driver for realizing quantum security group function
-firewall_driver = quantum.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver
-" | sudo tee -a /etc/quantum/plugins/openvswitch/ovs_quantum_plugin.ini
+# Firewall driver for realizing neutron security group function
+firewall_driver = neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver
+" | sudo tee -a /etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini
 
-sudo sed -i "s/# rabbit_host = localhost/rabbit_host = ${CONTROLLER_HOST}/g" /etc/quantum/quantum.conf
-sudo sed -i 's/# auth_strategy = keystone/auth_strategy = keystone/g' /etc/quantum/quantum.conf
-sudo sed -i "s/auth_host = 127.0.0.1/auth_host = ${CONTROLLER_HOST}/g" /etc/quantum/quantum.conf
-sudo sed -i 's/admin_tenant_name = %SERVICE_TENANT_NAME%/admin_tenant_name = service/g' /etc/quantum/quantum.conf
-sudo sed -i 's/admin_user = %SERVICE_USER%/admin_user = quantum/g' /etc/quantum/quantum.conf
-sudo sed -i 's/admin_password = %SERVICE_PASSWORD%/admin_password = quantum/g' /etc/quantum/quantum.conf
-sudo sed -i 's/^root_helper.*/root_helper = sudo/g' /etc/quantum/quantum.conf
+sudo sed -i "s/# rabbit_host = localhost/rabbit_host = ${CONTROLLER_HOST}/g" /etc/neutron/neutron.conf
+sudo sed -i 's/# auth_strategy = keystone/auth_strategy = keystone/g' /etc/neutron/neutron.conf
+sudo sed -i "s/auth_host = 127.0.0.1/auth_host = ${CONTROLLER_HOST}/g" /etc/neutron/neutron.conf
+sudo sed -i 's/admin_tenant_name = %SERVICE_TENANT_NAME%/admin_tenant_name = service/g' /etc/neutron/neutron.conf
+sudo sed -i 's/admin_user = %SERVICE_USER%/admin_user = neutron/g' /etc/quantum/neutron.conf
+sudo sed -i 's/admin_password = %SERVICE_PASSWORD%/admin_password = neutron/g' /etc/neutron/neutron.conf
+sudo sed -i 's/^root_helper.*/root_helper = sudo/g' /etc/neutron/neutron.conf
 
 echo "
 Defaults !requiretty
-quantum ALL=(ALL:ALL) NOPASSWD:ALL" | tee -a /etc/sudoers
+neutron ALL=(ALL:ALL) NOPASSWD:ALL" | tee -a /etc/sudoers
 
-# Restart Quantum Services
-service quantum-plugin-openvswitch-agent restart
+# Restart Neutron Services
+service neutron-plugin-openvswitch-agent restart
 
 
 # Clobber the nova.conf file with the following
@@ -152,21 +152,21 @@ ec2_dmz_host=${MYSQL_HOST}
 ec2_private_dns_show_ip=True
 
 # Network settings
-network_api_class=nova.network.quantumv2.api.API
-quantum_url=http://${MY_IP}:9696
-quantum_auth_strategy=keystone
-quantum_admin_tenant_name=service
-quantum_admin_username=quantum
-quantum_admin_password=quantum
-quantum_admin_auth_url=http://${MY_IP}:35357/v2.0
+network_api_class=nova.network.neutronv2.api.API
+neutron_url=http://${MY_IP}:9696
+neutron_auth_strategy=keystone
+neutron_admin_tenant_name=service
+neutron_admin_username=neutron
+neutron_admin_password=neutron
+neutron_admin_auth_url=http://${MY_IP}:35357/v2.0
 libvirt_vif_driver=nova.virt.libvirt.vif.LibvirtHybridOVSBridgeDriver
 linuxnet_interface_driver=nova.network.linux_net.LinuxOVSInterfaceDriver
 firewall_driver=nova.virt.libvirt.firewall.IptablesFirewallDriver
-security_group_api=quantum
+security_group_api=neutron
 
 #Metadata
-service_quantum_metadata_proxy = True
-quantum_metadata_proxy_shared_secret = foo
+service_neutron_metadata_proxy = True
+neutron_metadata_proxy_shared_secret = foo
 #metadata_host = ${MY_IP}
 #metadata_listen = 127.0.0.1
 #metadata_listen_port = 8775

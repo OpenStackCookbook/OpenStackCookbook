725ea54aad074e78d107dd12fbf9c89a3aac9910
Cody Bunch <bunchc@gmail.com>
2014-06-24 12:23:50 -0400

    auto-import into ldap

--- ldap.sh
+++ ldap.sh
@@ -10,7 +10,7 @@ slapd    slapd/internal/adminpw    password	openstack
 slapd    slapd/password1    password	openstack
 " | sudo debconf-set-selections
 
-sudo apt-get install -y slapd ldap-utils
+sudo apt-get install -y slapd ldap-utils expect
 
 # Check that it's working
 sudo slapcat 
@@ -40,6 +40,7 @@ echo "ou: Groups" >> $LDIF
 echo "description: generic groups branch" >> $LDIF
 echo -e "\n" >> $LDIF
 
+# Roles
 for line in `keystone role-list | awk '($4 != "name") && ($4 != "") {print $4}'`
 do
 	CN=$line
@@ -49,6 +50,7 @@ do
 	echo -e "\n" >> $LDIF
 done
 
+# Users
 for line in `keystone user-list | awk '($4 != "name") && ($4 != "") {print $4}'`
 do
 	CN=$line
@@ -59,6 +61,7 @@ do
 	echo -e "\n" >> $LDIF
 done
 
+# Tenants
 for line in `keystone tenant-list | awk '($4 != "name") && ($4 != "") {print $4}'`
 do
 	CN=$line
@@ -69,4 +72,12 @@ do
 	echo -e "\n" >> $LDIF
 done
 
-cat $LDIF
+# Import those into LDAP
+expect<<EOF
+spawn ldapadd -x -D cn=admin,dc=cook,dc=book -W -f /tmp/cookbook.ldif
+expect "Enter LDAP Password:"
+send "openstack\n"
+expect eof
+EOF
+
+# Configure Keystone for LDAP

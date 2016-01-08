6f6e39e79ce99725425075da10caed8c5084366d
Kevin Jackson <uksysadmin@gmail.com>
2016-01-08 09:50:17 +0000

    Forgot to add the first apt-get update before the first install in common.sh

--- common.sh
+++ common.sh
@@ -42,6 +42,7 @@ export MONGO_KEY=MongoFoo
 export OS_CACERT=/vagrant/ca.pem
 export OS_KEY=/vagrant/cakey.pem
 
+sudo apt-get update
 sudo apt-get install -y software-properties-common ubuntu-cloud-keyring
 sudo add-apt-repository -y cloud-archive:juno
 sudo apt-get update && sudo apt-get upgrade -y
